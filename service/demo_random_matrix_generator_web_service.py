16a1d4f3aa9df1f0c086f5d75b6b4520265acdb3
Benton Drew <benton.s.drew@drewantech.com>
2017-03-17 16:43:56 -0700

    Switch server from providing a page to hosting a web restful api.

--- service/demo_random_matrix_generator_web_service.py
+++ service/demo_random_matrix_generator_web_service.py
@@ -6,7 +6,8 @@ from flask import (Flask,
                    render_template,
                    flash,
                    redirect,
-                   url_for)
+                   url_for,
+                   jsonify)
 from os import urandom
 
 app = Flask(__name__)
@@ -35,6 +36,30 @@ def database_initialization():
   return redirect(url_for('index'))
 
 
+@app.route('/create_matrix_database', methods=['GET'])
+def create_matrix_database():
+  try:
+    from demo_random_matrix_generator.database_initialization \
+        import (create_database)
+    create_database()
+    return jsonify({'status': 'Successfully created matrix database.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in creating matrix database: {}'
+                   .format(e)}), 500
+
+
+@app.route('/create_matrix_tables', methods=['GET'])
+def create_matrix_tables():
+  try:
+    from demo_random_matrix_generator.database_initialization \
+        import (create_tables)
+    create_tables()
+    return jsonify({'status': 'Successfully created matrix tables.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in creating matrix tables: {}'
+                   .format(e)}), 500
+
+
 @app.route('/database_tear_down')
 def database_tear_down():
   try:
@@ -50,3 +75,27 @@ def database_tear_down():
   except Exception as e:
     raise flash('Error in tearing down databases: {}'.format(e))
   return redirect(url_for('index'))
+
+
+@app.route('/drop_matrix_database', methods=['GET'])
+def drop_matrix_database():
+  try:
+    from demo_random_matrix_generator.database_tear_down \
+        import (drop_database)
+    drop_database()
+    return jsonify({'status': 'Successfully dropped matrix database.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in dropping matrix database: {}'
+                   .format(e)}), 500
+
+
+@app.route('/drop_matrix_tables', methods=['GET'])
+def drop_matrix_tables():
+  try:
+    from demo_random_matrix_generator.database_tear_down \
+        import (drop_tables)
+    drop_tables()
+    return jsonify({'status': 'Successfully dropped matrix tables.'}), 201
+  except Exception as e:
+    return jsonify({'status': 'Error in dropping matrix tables: {}'
+                   .format(e)}), 500
